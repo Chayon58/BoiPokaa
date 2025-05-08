2fd0ea5a7d5591a47f6ce047fcf5974ea70ab8cb
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-05-08 22:03:58 +0600

    added router

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,13 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello World</div>,
-    
+
   },
 ]);
 
 
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
